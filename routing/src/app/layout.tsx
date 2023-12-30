568d6eace5cbff1a1585e4e228981dea52c711f7
Amaré Ngobeni <97952201+i-amare@users.noreply.github.com>
2023-12-30 19:47:05 +0200

    finishing touches

--- routing/src/app/layout.tsx
+++ routing/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,11 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={inter.className}>
 				<main className='flex min-h-screen flex-col items-center p-24'>
+					<div className='flex gap-2'>
+						<Link href='/about'>About</Link>
+						<Link href='/'>Home</Link>
+						<Link href='/portfolio'>Portfolio</Link>
+					</div>
 					{children}
 				</main>
 			</body>
