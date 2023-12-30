931ac46b8db0bde71d2c7f77a3f45f63755ae602
Amaré Ngobeni <97952201+i-amare@users.noreply.github.com>
2023-12-30 19:37:28 +0200

    programmatic routing

--- routing/src/app/portfolio/page.tsx
+++ routing/src/app/portfolio/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Portfolio() {
 	const projects = [
@@ -16,10 +19,22 @@ export default function Portfolio() {
 		},
 	];
 
+	const router = useRouter();
+	console.log(router);
+
+	function loadProject(id: number) {
+		const project = projects.find(
+			({ projectName, projectID }) => projectID === id
+		);
+
+		if (project)
+			router.push(`portfolio/${project.projectName}?id=${project.projectID}`);
+	}
+
 	return (
 		<div>
 			<h1 className='text-4xl font-bold'>Portfolio</h1>
-			<div role='list' className='flex flex-col'>
+			<div role='list' className='flex gap-2 flex-col'>
 				{projects.map(
 					({
 						projectName,
@@ -28,9 +43,20 @@ export default function Portfolio() {
 						projectName: string;
 						projectID: number;
 					}) => (
-						<Link href={`portfolio/${projectName}?id=${projectID}`}>
-							{projectName}
-						</Link>
+						<div
+							key={`${projectID}-${projectName}`}
+							className='flex flex-col text-center'
+						>
+							<Link href={`portfolio/${projectName}?id=${projectID}`}>
+								{projectName}
+							</Link>
+							<button
+								onClick={() => loadProject(projectID)}
+								className='px-2 py-1 rounded-sm bg-slate-400 text-black font-semibold'
+							>
+								Load Project
+							</button>
+						</div>
 					)
 				)}
 			</div>
