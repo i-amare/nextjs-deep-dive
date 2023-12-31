0488a3d71e0fb6e484cd9c071e24d30b9b9c5fe9
Amaré Ngobeni <97952201+i-amare@users.noreply.github.com>
2023-12-31 16:08:45 +0200

    added all events

--- events-app/app/events/page.tsx
+++ events-app/app/events/page.tsx
@@ -1,3 +1,16 @@
-export default function Events() {
-	return <main></main>
+import { getAllEvents } from '@/data';
+import { EventItem } from '../components/event-item';
+
+export default function AllEvents() {
+  const allEvents = getAllEvents();
+
+  return (
+    <main className='flex min-h-screen flex-col items-center p-4 lg:p-24'>
+      <ul className='flex w-full flex-col gap-8'>
+        {allEvents.map((value, index) => (
+          <EventItem event={value} key={`${value.title}-${Math.random()}`} />
+        ))}
+      </ul>
+    </main>
+  );
 }
