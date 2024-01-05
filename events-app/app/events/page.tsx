c1eee8d3680a345623132c9c611f5b400c36843a
Amaré Ngobeni <97952201+i-amare@users.noreply.github.com>
2024-01-05 15:27:44 +0200

    added search

--- events-app/app/events/page.tsx
+++ events-app/app/events/page.tsx
@@ -1,13 +1,25 @@
-import { getAllEvents } from '@/data';
+'use client';
+
+import { getAllEvents, getFilteredEvents } from '@/data';
 import { EventItem } from '../components/event-item';
 
-export default function AllEvents() {
-  const allEvents = getAllEvents();
+export default function AllEvents({
+  searchParams,
+}: {
+  searchParams: { year: string; month: string };
+}) {
+  const events =
+    searchParams.month && searchParams.year
+      ? getFilteredEvents(
+          parseInt(searchParams.year),
+          parseInt(searchParams.month),
+        )
+      : getAllEvents();
 
   return (
     <main className='flex min-h-screen flex-col items-center px-4 lg:px-24'>
       <ul className='flex w-full flex-col gap-8'>
-        {allEvents.map((value, index) => (
+        {events.map((value, index) => (
           <EventItem event={value} key={`${value.title}-${Math.random()}`} />
         ))}
       </ul>
